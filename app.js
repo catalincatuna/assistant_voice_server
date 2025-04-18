cad25ba5a916aa4968d5c9a77bc776e1a525547b
catalincatuna <catalin_catuna@yahoo.ro>
2025-04-18 20:51:48 +0300

    updated prompts and parameters for tools

--- app.js
+++ app.js
@@ -13,8 +13,38 @@ const port = 3000;
 const key = process.env.OPENAI_API_KEY;
 
 
-const SYSTEM_PROMPT =
-  "Esti un asistent care raspunde la intrebari legate de proprietatea urmatoare The Episode Jacuzzi Penthouses se afla in ClujNapoca la 15 minute de mers pe jos de EXPO Transilvania si ofera WiFi gratuit o terasa si parcare privata gratuita Proprietatea se afla la 33 km de Muzeul Etnografic al Transilvaniei si include vedere la oras si la piscinaAcest apartament cu aer conditionat are 1 dormitor un living o bucatarie complet utilata cu frigider si cafetiera precum si 1 baie cu bideu si dus Baia este dotata cu cada cu hidromasaj si articole de toaleta gratuite Exista de asemenea prosoape si lenjerie de patAcest apartament ofera o cada cu hidromasaj The Episode Jacuzzi Penthouses ofera un gratarThe Episode Jacuzzi Penthouses se afla la 38 km de Palatul Banffy si la 48 km de Cluj Arena Aeroportul International Avram Iancu Cluj se afla la 4 kmCuplurile apreciaza in mod deosebit aceasta locatie Iau dat scorul 98 pentru un sejur pentru 2 persoane";
+const SYSTEM_PROMPT1 = `
+Esti un asistent cu accent roman care deschide conversatia si intreaba 'cu ce va pot ajuta', 
+si ulterior raspunde scurt la intrebari legate de proprietatea urmatoare: 
+The Episode Jacuzzi Penthouses se afla in ClujNapoca la 15 minute de mers pe jos de EXPO Transilvania 
+si ofera WiFi gratuit o terasa si parcare privata gratuita. 
+Proprietatea se afla la 33 km de Muzeul Etnografic al Transilvaniei si include vedere la oras si la piscina.
+Acest apartament cu aer conditionat are 1 dormitor un living o bucatarie complet utilata cu frigider si cafetiera 
+precum si 1 baie cu bideu si dus. Baia este dotata cu cada cu hidromasaj si articole de toaleta gratuite. 
+Exista de asemenea prosoape si lenjerie de pat.
+Acest apartament ofera o cada cu hidromasaj. 
+The Episode Jacuzzi Penthouses ofera un gratar.
+The Episode Jacuzzi Penthouses se afla la 38 km de Palatul Banffy si la 48 km de Cluj Arena. 
+Aeroportul International Avram Iancu Cluj se afla la 4 km.
+Cuplurile apreciaza in mod deosebit aceasta locatie. Iau dat scorul 98 pentru un sejur pentru 2 persoane.
+Raspunsurile tale sa fie scurte si la subiect, daca trec 2 minute si clientul nu are nicio intrebare legata de proprietate poti incheia apelul. 
+The Episode Jacuzzi il poti pronunta cu accent romanesc.
+`;
+
+const SYSTEM_PROMPT2 = `
+IMPORTANT: Discutiile o sa fie in romana, daca clientul vorbeste in engleza te rugam sa raspunzi in engleza.
+IMPORTANT: Cum incepe sesiunea trebuie sa spui "Buna ziua! Sunt operatorul apartamentului Episode - Jacuzzi Penthouses. Cu ce va pot ajuta?"
+Lucrezi ca si operator la un apartament in regim hotelier si o sa primesti apeluri de la potentiali clienti.
+
+Dupa ce incepi conversatia, raspunzi la intrebarile clientului despre proprietate.
+
+Proprietatea are urmatoarea descriere: se află în Cluj-Napoca, la 15 minute de mers pe jos de EXPO Transilvania, și oferă WiFi gratuit, o terasă și parcare privată gratuită. Proprietatea se află la 3,3 km de Muzeul Etnografic al Transilvaniei și include vedere la oraș și la piscină.
+
+Trebuie sa discuti despre proprietate si nu despre altceva. Daca clientul are o rezervare va trebui sa o indentifici si sa identifici si clientul.
+
+Daca clientul cere sa faca o rezervare il vei directiona spre numaru : 0751020322
+`;
+
 
 // An endpoint which would work with the client code above - it returns
 // the contents of a REST API request to this protected endpoint
@@ -28,11 +58,12 @@ app.get("/session", async (req, res) => {
     },
     body: JSON.stringify({
       model: "gpt-4o-realtime-preview-2024-12-17",
-      voice: "verse",
-      instructions: SYSTEM_PROMPT,
+      voice: "ash",
+      instructions: SYSTEM_PROMPT2,
       modalities: ["audio", "text"],
       input_audio_transcription: {
-        model: "whisper-1",
+        model: "gpt-4o-mini-transcribe",
+        language: "ro",
       },
       tools: [
         {
@@ -51,7 +82,26 @@ app.get("/session", async (req, res) => {
             required: ["should_end"],
           },
         },
+        {
+          type: "function",
+          name: "get_reservation",
+          description:
+            "Daca clientul are o rezervare va trebui sa o indentifici si sa identifici si clientul, intreaba numele clientului",
+          parameters: {
+            type: "object",
+            properties: {
+              should_end: {
+                type: "string",
+                description: "numele clientului",
+              },
+            },
+            required: ["name"],
+          },
+        },
       ],
+      tool_choice: "auto",
+      temperature: 0.7,
+      max_response_output_tokens: 200,
     }),
   });
 
