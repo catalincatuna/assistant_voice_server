f84532837e13b080fba1fb725283c72cd1ea83c9
catalincatuna <catalin_catuna@yahoo.ro>
2025-04-19 12:41:57 +0300

    changed listening port to include also other connections

--- app.js
+++ app.js
@@ -149,6 +149,6 @@ app.post("/property", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running at http://0.0.0.0:${port}`);
+});
